ed37fa1da1a48d2dd45db06e3e4802ab97b8c8e0
Victor <platonov.ivanovich@gmail.com>
2024-09-07 19:45:52 -0700

    update innitial

---
+++ index.js
@@ -0,0 +1,11 @@
+/* Place your JavaScript in this file */
+        document.addEventListener('DOMContentLoaded', (event) => {
+            // Get the button element by its ID
+            const button = document.getElementById('button');
+
+            // Add a click event listener to the button
+            button.addEventListener('click', () => {
+                // Redirect the user to the desired URL
+                window.location.href = 'https://discord.gg/Aka6NZy2mF';
+            });
+        });
